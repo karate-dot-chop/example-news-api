15f5819b9a7b5f95ac513d6e6c9f694dc783e062
dani <dani.zaghian@gmail.com>
2021-05-26 13:49:27 -0700

    adds api key to encrypted credentials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.headers("X-Api-Key" => "your-api-key").get("https://newsapi.org/v2/everything?q=#{params[:search]}")
+    response = HTTP.headers("X-Api-Key" => Rails.application.credentials.news_api_key).get("https://newsapi.org/v2/everything?q=#{params[:search]}")
     render json: response.parse(:json)["articles"]
   end
 end
